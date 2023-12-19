4eefa20e4d2a3e951049876c67d44a277f2e515a
Muhammad Faruq <55911972+muhammad-faruq@users.noreply.github.com>
2023-12-19 22:44:37 +0800

    updates

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { application } = require("express");
 const express = require("express");
 const {google} = require("googleapis");
 const path = require("path");
@@ -8,35 +9,94 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 let count = 0;
+let dislikes = 0;
+let ratio = 0;
+let client;
+let googleSheets;
+let spreadsheetId;
+let metaData;
 app.get("/", async (req, res) => {
   const auth = new google.auth.GoogleAuth({
     keyFile: "credentials.json",
     scopes: "https://www.googleapis.com/auth/spreadsheets"
   });
 
-  const client = await auth.getClient();
-  const googleSheets = google.sheets({ version: "v4", auth: client});
-  const spreadsheetId = "1tt4L3gC3fBnVReAOKkTa8F2m48-0qt0l4V4Afypxo-8";
-  const metaData = await googleSheets.spreadsheets.get({
+  client = await auth.getClient();
+  googleSheets = google.sheets({ version: "v4", auth: client});
+  spreadsheetId = "1tt4L3gC3fBnVReAOKkTa8F2m48-0qt0l4V4Afypxo-8";
+  metaData = await googleSheets.spreadsheets.get({
     auth,
     spreadsheetId,
   })
 
-  const getRows = await googleSheets.spreadsheets.values.get({
+  const getCount = await googleSheets.spreadsheets.values.get({
     auth,
     spreadsheetId,
     range: "count_sheet!A1:A1",
   });
-  count = getRows.data.values[0][0]
+  count = getCount.data.values[0][0]
+  const getDislikes = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: "count_sheet!B1:B1",
+  });
+  dislikes = getDislikes.data.values[0][0];
   console.log("Current Count: " + count);
-  res.render("index", {count: count});
+  console.log("Current Dislikes: " + dislikes);
+  console.log("what: " + count + dislikes);
+  ratio = (parseInt(count, 10) / (parseInt(count, 10) + parseInt(dislikes, 10))) * 100;
+  ratio = parseFloat(ratio).toFixed(2).toString();
+  console.log("First Render")
+  console.log("Current Ratio: " + ratio); 
+  res.render("index", {ratio: ratio});
 });
 
-app.post("/", async (req, res) => {
+app.post("/decrement", async (req, res) => {
   const auth = new google.auth.GoogleAuth({
     keyFile: "credentials.json",
     scopes: "https://www.googleapis.com/auth/spreadsheets"
   });
+  const client = await auth.getClient();
+  const googleSheets = google.sheets({ version: "v4", auth: client});
+  const spreadsheetId = "1tt4L3gC3fBnVReAOKkTa8F2m48-0qt0l4V4Afypxo-8";
+  while (dislikes < 10) {
+    const getRows = await googleSheets.spreadsheets.values.get({
+      auth,
+      spreadsheetId,
+      range: "count_sheet!B1:B1",
+    });
+    dislikes = getRows.data.values[0][0];
+    console.log("refetching dislike data");
+  }
+
+
+  dislikes = parseInt(dislikes, 10);
+  dislikes += 1;
+  dislikes = dislikes.toString();
+  await googleSheets.spreadsheets.values.update({
+    auth,
+    spreadsheetId,
+    range: "count_sheet!B1:B1",
+    valueInputOption: "USER_ENTERED",
+    resource: {
+      values: [[dislikes]]
+    },
+  });
+  ratio = (parseInt(count, 10) / (parseInt(count, 10) + parseInt(dislikes, 10))) * 100;
+  ratio = parseFloat(ratio).toFixed(2).toString();
+  console.log("decremented")
+  console.log(ratio);
+  res.render("index", {ratio, ratio});
+})
+
+app.post("/increment", async (req, res) => {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets"
+  });
+  const client = await auth.getClient();
+  const googleSheets = google.sheets({ version: "v4", auth: client});
+  const spreadsheetId = "1tt4L3gC3fBnVReAOKkTa8F2m48-0qt0l4V4Afypxo-8";
   while (count < 10) {
     const getRows = await googleSheets.spreadsheets.values.get({
       auth,
@@ -47,13 +107,6 @@ app.post("/", async (req, res) => {
     console.log("refetching data");
   }
 
-  const client = await auth.getClient();
-  const googleSheets = google.sheets({ version: "v4", auth: client});
-  const spreadsheetId = "1tt4L3gC3fBnVReAOKkTa8F2m48-0qt0l4V4Afypxo-8";
-  // const metaData = await googleSheets.spreadsheets.get({
-  //   auth,
-  //   spreadsheetId,
-  // })
 
   count = parseInt(count, 10);
   count += 1;
@@ -67,8 +120,20 @@ app.post("/", async (req, res) => {
       values: [[count]]
     },
   });
-  res.render("index", {count, count});
+  ratio = (parseInt(count, 10) / (parseInt(count, 10) + parseInt(dislikes, 10))) * 100;
+  ratio = parseFloat(ratio).toFixed(2).toString();
+  console.log("incremented")
+  console.log(ratio);
+  res.render("index", {ratio, ratio});
+
 });
+
+app.get("/get-ratio", (req, res) => {
+  const ratio = (parseInt(count, 10) / (parseInt(count, 10) + parseInt(dislikes, 10))) * 100;
+  const formattedRatio = parseFloat(ratio).toFixed(2).toString();
+  res.json({ ratio: formattedRatio });
+});
+
 
 app.listen(1337, (req, res) => console.log("running on 1337"));
 
